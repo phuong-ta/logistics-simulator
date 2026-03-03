4a48013a1015291534cc71c44fb15965ccf2a3f5
phuong <phuong.ta011290@gmail.com>
2026-03-03 03:02:47 +0200

    recover main.go to test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,37 +1,29 @@
 package main
 
 import (
+	"net/http"
+
 	"log"
 	"logistics-simulator/internal/database"
-	"logistics-simulator/internal/handlers"
 	"logistics-simulator/internal/workers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 1.init database connection and auto-migrate Order model
 	database.InitDB()
-
-	// 2. Create Channel for Worker Pool (Buffer 100)
+	log.Println("--- Kết nối thành công! App đã sẵn sàng cho bước tiếp theo ---")
+	// 2. create a channel to send order IDs to workers ( 100 buffer)
 	jobChan := make(chan uint, 100)
 
-	// 3. Run Worker Pool (3 workers)
-	workers.StartWorkerPool(3, jobChan, database.DB)
+	// 3. Khởi tạo Worker Pool với 5 thợ (Goroutines)
+	workers.StartWorkerPool(5, jobChan, database.DB)
 
-	// 4. init Handler
-	orderHandler := &handlers.OrderHandler{
-		DB:      database.DB,
-		JobChan: jobChan,
-	}
-
-	// 5. Setup Gin Router
 	router := gin.Default()
-
-	router.POST("/orders", orderHandler.CreateOrder)
-	router.GET("/orders", orderHandler.GetOrders)
-
-	// 6. Chạy Server ở port 8080
-	log.Println("Server is running on port 8080...")
-	router.Run(":8080")
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
